f8a596cd09ff66bfd8016b669e7d39f35cc8ce82
YachaTree <dnftksghkfkd@icloud.com>
2024-02-19 20:57:51 +0900

    feat: 1차 과제 업로드(2/19)

---
+++ src/main/java/com/Tree_dev/workout_done/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.Tree_dev.workout_done.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "1차 과제 제출");
+        return "hello";
+    }
+}
