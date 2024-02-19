c20eca39e075414f893618746e2c2484960e2441
YachaTree <dnftksghkfkd@icloud.com>
2024-02-19 20:24:54 +0900

    first commit

---
+++ src/main/java/com/Tree_dev/workout_done/WorkoutDoneApplication.java
@@ -0,0 +1,13 @@
+package com.Tree_dev.workout_done;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutDoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tree_dev/workout_done/WorkoutDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tree_dev.workout_done;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
